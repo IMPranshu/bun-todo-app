d2f132c9eac10f5e9340ab4845c7b4e698b18736
Pranshu Kumar Agrawal <pranshukumar1999@gmail.com>
2023-09-11 20:54:37 +0530

    added GET & POST requests in server

--- index.tsx
+++ index.tsx
@@ -1,12 +1,44 @@
+import { renderToString } from "react-dom/server";
+
 const server = Bun.serve({
   hostname: "localhost",
   port: 8080,
   fetch: handler,
 });
 
+type Todo = { id: number; text: string };
+const todos: Todo[] = [];
+
 console.log(`Listening on http://${server.hostname}:${server.port}`);
 
-function handler(request: Request): Response {
+async function handler(request: Request): Promise<Response> {
   const url = new URL(request.url);
+
+  if (url.pathname == "" || url.pathname === "/")
+    return new Response(Bun.file("index.html"));
+
+  if (url.pathname === "/todos" && request.method === "POST") {
+    const { todo } = await request.json();
+    if (!todo?.length) return new Response("Invalid Input", { status: 500 });
+    todos.push({
+      id: todos.length + 1,
+      text: todo,
+    });
+    return new Response(renderToString(<TodoList todos={todos} />));
+  }
+
+  if (url.pathname === "/todos" && request.method === "GET")
+    return new Response(renderToString(<TodoList todos={[]} />));
+
   return new Response("Not Found", { status: 404 });
+}
+
+function TodoList(props: { todos: Todo[] }) {
+  return (
+    <ul>
+      {props.todos.length
+        ? props.todos.map((todo) => <li key={todo.id}>{todo.text}</li>)
+        : "No TODOs added"}
+    </ul>
+  );
 }
